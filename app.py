18508ed36a8ed3d38d838d69807513ae3a90f7e4
moregan366 <moregan10@outlook.com>
2026-04-21 15:40:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -124,4 +124,6 @@ def get_cards():
 # Run app
 # ------------------------
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
